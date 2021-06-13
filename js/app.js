fc78feac3d35b3312bdf0f9023f207b2d9ba49c5
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2021-06-13 16:15:13 +0530

    Intial Setup

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const tableBody = document.querySelector(".table-body");
+const row = `<tr>
+<th scope="row">1</th>
+<td>Mark</td>
+<td>Otto</td>
+<td>https://github.com/Sachin-chaurasiya</td>
+</tr>`;
+
+window.onload = function fetchStudents() {
+  fetch("./js/data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((student) => {
+        tableBody.innerHTML += `<tr>
+        <th scope="row">${student.no}</th>
+        <td>${student.firstName}</td>
+        <td>${student.lastName}</td>
+        <td><a href=${student.gitHubUrl} class="btn btn-primary bg-gradient">View Profile</a></td>
+        </tr>`;
+      });
+    })
+    .catch((err) => console.log(err));
+  console.log(tableBody);
+};
