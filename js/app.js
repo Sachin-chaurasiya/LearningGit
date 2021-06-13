9cdbed2a6685597b63c8f158657cce8033396395
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2021-06-13 16:53:01 +0530

    added random number to roll number attribute

--- js/app.js
+++ js/app.js
@@ -12,10 +12,12 @@ window.onload = function fetchStudents() {
     .then((data) => {
       data.forEach((student) => {
         tableBody.innerHTML += `<tr>
-        <th scope="row">${student.no}</th>
+        <th scope="row">${Math.floor(Math.random() * 101) + 1}</th>
         <td>${student.firstName}</td>
         <td>${student.lastName}</td>
-        <td><a href=${student.gitHubUrl} class="btn btn-primary bg-gradient">View Profile</a></td>
+        <td><a href=${
+          student.gitHubUrl
+        } class="btn btn-primary bg-gradient">View Profile</a></td>
         </tr>`;
       });
     })
